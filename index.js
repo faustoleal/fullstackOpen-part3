93ad3c1ea089639687f5a8029495c9738ce80479
Fausto Leal <fausto.leal2099@gmail.com>
2024-06-10 12:13:44 -0300

    add dist

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 
 morgan.token("body", (request) => JSON.stringify(request.body));
 
+app.use(express.static("dist"));
 app.use(express.json());
 app.use(morgan(`tiny`));
 app.use(cors());
